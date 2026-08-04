bdbcfbb806d3ec4c9088a7f56194a02021062224
Juan Alvarez <juan@example.com>
2026-08-04 11:37:04 -0500

    fix: react-router.config sin routes hardcodeadas para dejar escaneo automatico de app/routes

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,13 +1,5 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: false, // Mantener en false para SPA
-  routes: [
-    {
-      path: "/*", // Todas las rutas se manejan desde Root
-      file: "app/root.tsx"
-    }
-  ]
+  ssr: false,
 } satisfies Config;
